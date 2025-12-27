d791c96ab1a7817970d292875a684d87ceef8ace
Luizaa027 <luizanazirbaeva@icloud.com>
2025-12-27 17:40:22 +0600

    a

--- aman/models.py
+++ aman/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Settings(models.Model):
+    title = models.CharField(max_length= 100 ,verbose_name="Название сайта")
+    description = models.TextField(verbose_name="Описание")
+    logo = models.ImageField(verbose_name="Логотип сайта")
+    icon = models.ImageField(verbose_name="Иконка сайта")
+    email = models.EmailField(verbose_name="Контактный email")
+    phone = models.CharField(max_length=20, verbose_name="Номер телефона")
+    locate = models.CharField(max_length=100 , verbose_name="Адрес")
+
+    
+
+    class Meta:
+        verbose_name = "Настройка сайта"
+        verbose_name_plural = "Настройки сайта"
--- aman/views.py
+++ aman/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from aman.models import Setting 
 
 # Create your views here.
+
+def index(request):
+    settings = Setttings.object.lates("id")
+    return render(request, "index.html", locals()) 
+
+def contacts(request):
+    settings = Settings.object.lates("id")
+    return render(request, "contacts.html",locals())
+
